31981fcb3d356bbb11ff072fa41b0ccbca5762c8
Victor <victor@sene.ovh>
2017-10-04 17:55:54 +0200

    Approche simpliste suivie de ligne

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,29 @@ import lejos.utility.Delay;
 public class Main {
     public static void main(String[] args){
         SwagBot robot = new SwagBot(MotorPort.B, MotorPort.C, SensorPort.S2, SensorPort.S3);
-        for (int i = 0; i < 100; i++) {
-            switch (robot.color()){
-                case Color.BLACK : System.out.println("Oui"); break;
-                default : System.out.println("Non");
+        int return_color;
+        boolean forward = false;
+        while (true){
+            return_color = robot.color();
+            if(return_color == Color.BLACK){
+                if (!forward){
+                    robot.forward();
+                    forward = true;
+                }
+            }else{
+                robot.stop();
+                robot.rotate((float) 0.1);
+                forward = false;
             }
-            Delay.msDelay(1000);
         }
+
+//        for (int i = 0; i < 100; i++) {
+//            switch (robot.color()){
+//                case Color.BLACK : System.out.println("Oui"); break;
+//                default : System.out.println("Non");
+//            }
+//            Delay.msDelay(1000);
+//        }
 
     }
 }
