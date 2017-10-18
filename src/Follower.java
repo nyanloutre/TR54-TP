c596ca80ac433441b63e669a7f92d6b78116a1ea
Victor <victor@sene.ovh>
2017-10-18 16:52:48 +0200

    print distance

--- src/Follower.java
+++ src/Follower.java
@@ -1,5 +1,13 @@
+import lejos.hardware.Button;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.hardware.port.SensorPort;
+
 public class Follower {
 
-
+    public static void main(String[] args) {
+        SwagBot robot = new SwagBot(MotorPort.B, MotorPort.C, SensorPort.S2, SensorPort.S1);
+        System.out.println("Obstacle à : " + robot.distance());
+    }
 }
 
--- src/SwagBot.java
+++ src/SwagBot.java
@@ -31,6 +31,15 @@ public class SwagBot{
         this.button_sensor = new EV3TouchSensor(port_button_sensor);
     }
 
+    public SwagBot(Port port_motor_A, Port port_motor_B, Port port_ultrasonic, Port port_button_sensor) {
+        this.motorA = new EV3LargeRegulatedMotor(port_motor_A);
+        this.motorB = new EV3LargeRegulatedMotor(port_motor_B);
+        this.ultrasonic = new EV3UltrasonicSensor(port_ultrasonic);
+        this.ultrasonic_provider = this.ultrasonic.getDistanceMode();
+        this.button_sensor = new EV3TouchSensor(port_button_sensor);
+    }
+
+
     public SwagBot(Port port_motor_A, Port port_motor_B) {
         this.motorA = new EV3LargeRegulatedMotor(port_motor_A);
         this.motorB = new EV3LargeRegulatedMotor(port_motor_B);
